ffc684719371f74423791df361a4c0b5fd452238
fukasawawa <blackcat4474@gmail.com>
2019-02-11 02:50:09 +0900

    add 名前を省略する機能を追加

---
+++ ProcessName.py
@@ -0,0 +1,30 @@
+import ChangeName, re
+
+def splitName(Name):
+    xName = re.split('[ -]', Name)
+    return xName
+
+def omitName(xName):
+    for i in range(len(xName)-1):
+        xName[i] = xName[i][0]
+    return xName
+
+def unionName(xName):
+    new_Name = xName[0]
+    if len(xName) <= 1:
+        return new_Name
+    for i in range(len(xName)-1):
+        new_Name += ". " + xName[i+1]
+    return new_Name
+
+
+def main():
+    english_name = ChangeName.main()
+    xName = splitName(english_name)
+    new_Name = unionName(omitName(xName))
+    for i in range(0):
+        print("A")
+    print(new_Name)
+# main関数呼び出し
+if __name__ == "__main__":
+    main()
