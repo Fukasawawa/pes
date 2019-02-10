a4f44dd86201fa4f7a8750c3117bc8ff4768d95c
fukasawawa <blackcat4474@gmail.com>
2019-02-11 02:49:19 +0900

    fix 無駄な変数を削除

--- ChangeName.py
+++ ChangeName.py
@@ -1,4 +1,4 @@
-import requests, webbrowser, re
+import requests, re
 import bs4
 
 def getName():
@@ -12,33 +12,27 @@ def getName():
     return original_name
 
 def searchName(Name):
-    url=[]
-    res = requests.get("https://google.com/search?q=" + Name + "+サッカー+wiki")
+    res = requests.get("https://google.com/search?q=" + Name + "+wiki")
     soup = bs4.BeautifulSoup(res.text, "lxml")
     url = soup.select(".r a")
-    #for i in range(5):
-    #    webbrowser.open("https://google.com" + link_elems[i].get("href"))
     return url
 
 def pickupName(url):
     res = requests.get("https://google.com" + url.get("href"))
     soup = bs4.BeautifulSoup(res.text, "lxml")
-    #for i in soup.find_all("p"):
-    #    for j in soup.find_all("b").string:
-    #        if(j != None):
-    #            print(j)
     name_list = soup.find_all("td", text=re.compile("[A-z]{2}"))
     #name_list.extend(soup.find_all("p", text=re.compile("[A-z]{2}")))
     name_list.sort(key=len, reverse=True)
     #print(name_list)
     #print(soup.find_all("p").find_all("b"))
-    return name_list[0].string
+    return name_list[0].string.split("\n")[1]
 
 def main():
     original_name = getName()
     wiki = searchName(original_name)
     #print("https://google.com" + wiki[0].get("href"))
-    print(pickupName(wiki[0]))
+    #print(pickupName(wiki[0]))
+    return pickupName(wiki[0])
 
 # main関数呼び出し
 if __name__ == "__main__":
